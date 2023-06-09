4a9ee5e93bdcdb2313135025a66e618812cfa052
CarlosMorales14 <morales.angel.carlos.alberto@gmail.com>
2023-06-09 03:12:22 -0600

    Avance vistas y base de datos CM

--- libreria/urls.py
+++ libreria/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('depresion/', views.depresion, name='depresion'),
+    path('alcoholismo/', views.alcoholismo, name='alcoholismo'),
+    path('MDQ/', views.mdq, name='mdq'),
+    path('DEP-ADO/', views.drogas, name='drogas'),
+    
+    
+    
 ]
--- libreria/views.py
+++ libreria/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Pregunta, Test
 # Create your views here.
 
 
@@ -8,4 +8,25 @@ def inicio(request):
    return render(request, 'index.html')
 
 def depresion(request):
-   return render(request, 'depresion.html')
+   #aqui va el test phq-9 xd
+   test_PHQ = Test.objects.get(nombre='PHQ-9')  # Recuperar el test 
+   preguntas = Pregunta.objects.filter(test=test_PHQ)  # Filtrar las preguntas solo para el test
+   return render(request, 'depresion.html', {'preguntas': preguntas}) # Mandar las preguntas al test para que se recorran en un for
+
+def mdq(request):
+   #aqui va el test mdq xd
+   test_MDQ = Test.objects.get(nombre='MDQ')  # Recuperar el test 
+   preguntas = Pregunta.objects.filter(test=test_MDQ)  # Filtrar las preguntas solo para el test
+   return render(request, 'mdq.html', {'preguntas': preguntas}) # Mandar las preguntas al test para que se recorran en un for
+
+def drogas(request):
+   #aqui va el test DEP-ADO xd
+   DEP_ADO = Test.objects.get(nombre='DEP-ADO')  # Recuperar el test 
+   preguntas = Pregunta.objects.filter(test=DEP_ADO)  # Filtrar las preguntas solo para el test
+   return render(request, 'drogas.html', {'preguntas': preguntas}) # Mandar las preguntas al test para que se recorran en un for
+
+def alcoholismo(request):
+   #aqui va el audit xd
+   test_audit = Test.objects.get(nombre='AUDIT')  # Recuperar el test AUDIT
+   preguntas = Pregunta.objects.filter(test=test_audit)  # Filtrar las preguntas solo para el test AUDIT
+   return render(request, 'alcoholismo.html', {'preguntas': preguntas})
